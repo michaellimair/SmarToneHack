89ff77d1c9651049deed9cc11b582362d20acf2f
michaellimair <michaellimbong@gmail.com>
2019-10-26 16:53:34 +0800

    init map

--- app/containers/Root/index.js
+++ app/containers/Root/index.js
@@ -1,15 +1,84 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState, useEffect, useRef} from 'react';
 import MapView from 'react-native-maps';
 import {styles} from './styles';
+import {PermissionsAndroid} from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
+
+const requestLocationPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        'title': 'Location Permission',
+        'message': 'Location Permission' +
+                   'so you can use GPS location.'
+      }
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log('GPS permission granted');
+      return true;
+    } else {
+      console.log('GPS location denied');
+      return false;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
 
 const Root = () => {
+  async function requestCurrentLocation() {
+    Geolocation.getCurrentPosition(
+      position => {
+        console.log("position is", position.coords);
+        return position.coords;
+      },
+      error => {
+        // See error code charts below.
+        console.log(error.code, error.message);
+      },
+      {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+    );
+  }
+
+  const [locationPermissionGranted, setLocationPermission] = useState(false);
+  const [userLocation, setUserLocation] = useState({
+    latitude: 0,
+    longitude: 0,
+  });
+  const mapReferenceContainer = useRef(null);
+
+  const _handleUserLocationChange = c => {
+    // userLocation = c;
+  }
+
+  useEffect(() => {
+    async function getInitialLocation() {
+      const isLocationGranted = await requestLocationPermission();
+      setLocationPermission(isLocationGranted);
+      if (isLocationGranted) {
+        const obtainedUserLocation = await requestCurrentLocation(); //initial location
+      }
+    }
+    getInitialLocation();
+  }, []);
+
   return (
     <MapView
       provider={'google'}
       style={styles.map}
+      ref={mapReferenceContainer}
+      initialRegion={{
+        latitude: 22.2591201,
+        longitude: 114.1320672,
+        latitudeDelta: 0.03,
+        longitudeDelta: 0.03,
+      }}
+      onUserLocationChange={c => _handleUserLocationChange(c)}
+      followsUserLocation={true}
+      showsUserLocation={true}
     />
-  )
+  );
 };
 
 export default Root;
