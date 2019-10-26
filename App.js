e613bb492fd0ed7623572138e8b554669668b5ce
MirrorCraze <crazemirror@gmail.com>
2019-10-26 22:32:58 +0800

    MainPage Completed

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React from 'react';
 import Root from './app/containers/Root';
+import MainPage from './app/containers/MainPage'
 import {
   SafeAreaView,
   StyleSheet,
@@ -26,7 +27,7 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
-  return <Root />;
+  return <MainPage />;
 };
 
 // const App: () => React$Node = () => {
---
+++ app/containers/MainPage/styles.js
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+/*export const body_button = StyleSheet.create({
+
+});*/
+export const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+  },
+});
